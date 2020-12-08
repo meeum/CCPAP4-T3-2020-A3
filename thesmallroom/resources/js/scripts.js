c8a05667723c2c25a1974ffe763ba119024b4e33
meeum <sam@meeum.com>
2020-12-08 22:35:03 +1100

    Added Caitlyn's site

---
+++ thesmallroom/resources/js/scripts.js
@@ -0,0 +1,13 @@
+function openNav() {
+
+    let toggler = document.querySelector('.nav-prompt');
+    let navigation = document.querySelector('.nav-inner'); 
+
+    toggler.onclick = function() {
+
+        navigation.classList.toggle('open'); 
+
+    };
+} 
+
+openNav();
