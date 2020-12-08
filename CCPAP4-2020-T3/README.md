a9dcbcbf4110856e53811f14b3382b2ea3d0d7f3
meeum <sam@meeum.com>
2020-12-08 22:07:05 +1100

    Add Bec site

---
+++ CCPAP4-2020-T3/README.md
@@ -0,0 +1,2 @@
+# CCPAP4-2020-T3
+new repo for web design 
---
+++ CCPAP4-2020-T3/resources/js/scripts.js
@@ -0,0 +1,11 @@
+function openNav( ) {
+
+    let toggler = document.querySelector('.nav-prompt');
+    let navigation = document.querySelector('.nav-inner');
+
+    toggler.onclick = function ( ) {
+        navigation.classList.toggle( 'open' )
+    };
+}
+
+openNav();
