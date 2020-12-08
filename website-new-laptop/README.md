96d7ae3f67f4fdc1114ce7ee7aa95c56f279cf1f
meeum <sam@meeum.com>
2020-12-08 21:56:39 +1100

    Add files via upload

---
+++ website-new-laptop/README.md
@@ -0,0 +1,2 @@
+# website-new-laptop
+The website for my assignment on my new laptop. 
---
+++ website-new-laptop/resources/js/scripts.js
@@ -0,0 +1,16 @@
+/* this is to open and close the nav*/
+
+function openNav () {
+
+    let toggler = document.querySelector('.nav-prompt');
+    let navigation = document.querySelector('.nav-inner');
+
+    toggler.onclick = function () {
+        navigation.classList.toggle('open');
+    };
+}
+
+openNav();
+
+//this is for the gallery
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
